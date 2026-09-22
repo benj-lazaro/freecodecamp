bc22a94dad488e0200f21c8083ab5c6a01cd5d43
benj-lazaro <benjie.work@gmail.com>
2026-09-22 21:06:56 +0800

    build a greeting bot step 7

--- javascript/01-build-a-greeting-bot/app.js
+++ javascript/01-build-a-greeting-bot/app.js
@@ -6,3 +6,6 @@ bot = "teacherBot";
 let botLocation = "the universe";
 
 console.log("Allow me to introduce myself.");
+
+let botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
