f4947a8f953bb090321c6179862add418c77ba44
benj-lazaro <benjie.work@gmail.com>
2026-09-22 21:21:59 +0800

    build a greeting bot step 12

--- javascript/01-build-a-greeting-bot/app.js
+++ javascript/01-build-a-greeting-bot/app.js
@@ -19,3 +19,7 @@ const nicknameIntroduction = "My nickname is " + bot + ".";
 console.log(nicknameIntroduction);
 
 bot = "awesomeTeacherBot";
+
+const newNicknameGreeting =
+  "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
