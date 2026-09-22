d3a5c11c0783c5027dce79164f8cb5e8791620f2
benj-lazaro <benjie.work@gmail.com>
2026-09-22 21:55:24 +0800

    02 build a javascript trivia bot

---
+++ javascript/02-build-a-js-trivia-bot/code/app.js
@@ -0,0 +1,29 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Trivia Bot";
+const botLocation = "the Internet";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  favoriteLanguage + " is one of the most popular programming languages today.";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage + " was once considered a toy programming language.";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage +
+  " reputation as a toy language is now considered ancient history.";
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
